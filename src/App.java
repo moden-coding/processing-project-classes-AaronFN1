fd081f377c6b1ef685d7383ac20db3fb576a0922
Aaron <26afreedmannaditch@gannacademy.org>
2025-05-15 09:23:37 -0400

    Got in a class but trying to rotate it now

--- src/App.java
+++ src/App.java
@@ -5,33 +5,11 @@ public class App extends PApplet {
         PApplet.main("App");
     }
 
-    int shipX;
-    int shipY;
-    double shipXVelocity;
-    double shipYVelocity;
-    double shipVelocity;
-    double shipXAcceleration;
-    double shipYAcceleration;
-    double shipAcceleration;
-    int xDirection;
-    int yDirection;
-    double maxVelocity;
+    Ship ship;
 
     public void setup() {
-
         background(0);
-        shipX = 600;
-        shipY = 400;
-        shipXVelocity = 0;
-        shipYVelocity = 0;
-        shipVelocity = 5;
-        shipXAcceleration = 0;
-        shipYAcceleration = 0;
-        shipAcceleration = 0.2;
-        yDirection = 0;
-        xDirection = 0;
-        maxVelocity = 33;
-
+        ship = new Ship(600, 400, 5, .2, 40, 35, 70, this);
     }
 
     public void settings() {
@@ -40,98 +18,28 @@ public class App extends PApplet {
 
     public void draw() {
         background(0);
-        rect(shipX, shipY, 70, 70);
-        if (abs(xDirection) > 1){
-        shipXVelocity += shipXAcceleration;
-        }
-        if (abs(yDirection) > 1){
-        shipYVelocity += shipYAcceleration;
-        }
-        shipX += shipXVelocity;
-        shipY += shipYVelocity;
-        //#region acceleration velocity and direction resetting
-        if (yDirection < 0 && shipYVelocity > 0) {
-            shipYAcceleration = 0;
-            shipYVelocity = 0;
-        }
-        if (yDirection > 0 && shipYVelocity < 0) {
-            shipYAcceleration = 0;
-            shipYVelocity = 0;
-        }
-        if (xDirection < 0 && shipXVelocity > 0) {
-            shipXAcceleration = 0;
-            shipXVelocity = 0;
-        }
-        if (xDirection > 0 && shipXVelocity < 0) {
-            shipXAcceleration = 0;
-            shipXVelocity = 0;
-        }
-        if (shipYVelocity == 0) {
-            yDirection = 0;
-            shipYAcceleration = 0;
-        }
-        if (shipXVelocity == 0) {
-            xDirection = 0;
-            shipXAcceleration = 0;
-        }
-        //#endregion
-        // #region ship edge reset
-        if (shipY > 800 + 35) {
-            shipY = 0 - 35;
-        }
-        if (shipY < 0 - 70) {
-            shipY = 800 + 35;
-        }
-        if (shipX > 1200 + 35) {
-            shipX = 0 - 35;
-        }
-        if (shipX < 0 - 70) {
-            shipX = 1200 + 35;
-        }
-        // #endregion
+        ship.display();
+        ship.movement();
     }
 
     public void mousePressed() {
-        System.out.println("X velocity = " + shipXVelocity);
-        System.out.println("Y velocity = " + shipYVelocity);
+        // System.out.println("X velocity = " + shipXVelocity);
+        // System.out.println("Y velocity = " + shipYVelocity);
     }
 
     public void keyPressed() {
         if (key == 'w' || keyCode == UP) {
-            if (shipYVelocity > -maxVelocity) {
-                shipYVelocity -= shipVelocity;
-                yDirection--;
-            }
-            if (yDirection < 0) {
-                shipYAcceleration = shipAcceleration;
-            }
+            ship.moveUp();
         }
         if (key == 's' || keyCode == DOWN) {
-            if (shipYVelocity < maxVelocity) {
-                shipYVelocity += shipVelocity;
-                yDirection++;
-            }
-            if (yDirection > 0) {
-                shipYAcceleration = -shipAcceleration;
-            }
+            ship.moveDown();
+            ship.rotate(45);
         }
         if (key == 'a' || keyCode == LEFT) {
-            if (shipXVelocity > -maxVelocity) {
-                shipXVelocity -= shipVelocity;
-                xDirection--;
-            }
-            if (xDirection < 0) {
-                shipXAcceleration = shipAcceleration;
-            }
+            ship.moveLeft();
         }
         if (key == 'd' || keyCode == RIGHT) {
-            if (shipXVelocity < maxVelocity) {
-                shipXVelocity += shipVelocity;
-                xDirection++;
-            }
-            if (xDirection > 0) {
-                shipXAcceleration = -shipAcceleration;
-            }
+            ship.moveRight();
         }
     }
 }
--- src/Ship.java
+++ src/Ship.java
@@ -1,6 +1,7 @@
 import processing.core.PApplet;
 
 public class Ship {
+    private PApplet canvas;
     private int x;
     private int y;
     private double xVelocity;
@@ -14,8 +15,9 @@ public class Ship {
     private double maxVelocity;
     private int xSize;
     private int ySize;
-    
-    public Ship(int shipX, int shipY, double shipVelocity, double shipAcceleration, double shipMaxVelocity, int shipXSize, int shipYSize, PApplet c){
+
+    public Ship(int shipX, int shipY, double shipVelocity, double shipAcceleration, double shipMaxVelocity,
+            int shipXSize, int shipYSize, PApplet c) {
         canvas = c;
         x = shipX;
         y = shipY;
@@ -24,9 +26,109 @@ public class Ship {
         maxVelocity = shipMaxVelocity;
         xSize = shipXSize;
         ySize = shipYSize;
+        xVelocity = 0;
+        yVelocity = 0;
+        xDirection = 0;
+        yDirection = 0;
+        xAcceleration = 0;
+        yAcceleration = 0;
     }
-    
-    public void display(){
-        canvas.rect
+
+    public void display() {
+        canvas.fill(255);
+        canvas.rect(x, y, xSize, ySize);
     }
+
+    //#region moving
+    public void moveUp() {
+        if (yVelocity > -maxVelocity) {
+            yVelocity -= velocity;
+            yDirection--;
+        }
+        if (yDirection < 0) {
+            yAcceleration = acceleration;
+        }
+    }
+
+    public void moveDown() {
+        if (yVelocity < maxVelocity) {
+            yVelocity += velocity;
+            yDirection++;
+        }
+        if (yDirection > 0) {
+            yAcceleration = -acceleration;
+        }
+    }
+
+    public void moveLeft() {
+        if (xVelocity > -maxVelocity) {
+            xVelocity -= velocity;
+            xDirection--;
+        }
+        if (xDirection < 0) {
+            xAcceleration = acceleration;
+        }
+    }
+
+    public void moveRight() {
+        if (xVelocity < maxVelocity) {
+            xVelocity += velocity;
+            xDirection++;
+        }
+        if (xDirection > 0) {
+            xAcceleration = -acceleration;
+        }
+    }
+
+    public void movement() {
+        if (xDirection > 1 || xDirection <-1) {
+            xVelocity += xAcceleration;
+        }
+        if (yDirection > 1 || yDirection <-1) {
+            yVelocity += yAcceleration;
+        }
+        x += xVelocity;
+        y += yVelocity;
+        // #region acceleration velocity and direction resetting
+        if (yDirection < 0 && yVelocity > 0) {
+            yAcceleration = 0;
+            yVelocity = 0;
+        }
+        if (yDirection > 0 && yVelocity < 0) {
+            yAcceleration = 0;
+            yVelocity = 0;
+        }
+        if (xDirection < 0 && xVelocity > 0) {
+            xAcceleration = 0;
+            xVelocity = 0;
+        }
+        if (xDirection > 0 && xVelocity < 0) {
+            xAcceleration = 0;
+            xVelocity = 0;
+        }
+        if (yVelocity == 0) {
+            yDirection = 0;
+            yAcceleration = 0;
+        }
+        if (xVelocity == 0) {
+            xDirection = 0;
+            xAcceleration = 0;
+        }
+        // #endregion
+        // #region ship edge reset
+        if (y > 800 + ySize / 2) {
+            y = 0 - ySize / 2;
+        }
+        if (y < 0 - ySize) {
+            y = 800 + ySize / 2;
+        }
+        if (x > 1200 + xSize / 2) {
+            x = 0 - xSize / 2;
+        }
+        if (x < 0 - xSize) {
+            x = 1200 + xSize / 2;
+        }
+        // #endregion
+    }
+    //#endregion
 }
